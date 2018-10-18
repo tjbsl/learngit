5605ae8ddb27ec797cf64d0d43e8fd5a4b8631f0
Bian_Shaolei <tjbsl@126.com>
2018-10-18 11:18:30 +0800

    本地代码bug修改

--- Git_Learn/src/com/tjbsl/Main.java
+++ Git_Learn/src/com/tjbsl/Main.java
@@ -9,11 +9,12 @@ public class Main {
 		System.out.println("3 -master");
 		System.out.println("4 -master");
 		System.out.println("5 -master");
-		System.out.println("master end");
+
 		System.out.println("6 -master");
 		System.out.println("7 -master");
 		System.out.println("8 -master");
 		System.out.println("9 -master");
 		System.out.println("10 -master");
+		System.out.println("master end");
 	}
 }
