a565079ef15d75b597831145b3b9ec574c1e7aee
Bian_Shaolei <tjbsl@126.com>
2018-10-18 11:01:54 +0800

    项目提交

---
+++ Git_Learn/src/com/tjbsl/Main.java
@@ -0,0 +1,14 @@
+package com.tjbsl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("master start");
+		System.out.println("1 -master");
+		System.out.println("2 -master");
+		System.out.println("3 -master");
+		System.out.println("4 -master");
+		System.out.println("5 -master");
+		System.out.println("master end");
+	}
+}
